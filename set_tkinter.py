ce167b169ebfd63cf7fb8008d5e17eeb3e9a8165
wenting <[wentingmen@163.com]>
2020-01-06 15:20:22 +0800

    项目准备，初识tkinter

--- set_tkinter.py
+++ set_tkinter.py
@@ -1,9 +1,15 @@
 import tkinter
+from tkinter import ttk
 
 root = tkinter.Tk()
 
-label = tkinter.Label(root, text='kello world')
+#设置按钮
+tkinter.Button(root, text='Click me').pack()
+ttk.Button(root, text='Click me').pack()
 
-label.pack()
+#设置标签
+#label = tkinter.Label(root, text='kello world')
+#label.pack()
+
 
 root.mainloop()
